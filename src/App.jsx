491ffffc6ad22b360f576ec94d922ef0faedc3d8
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2024-08-27 22:57:14 +0300

    added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
         <Feedbacks />
         <div className='relative z-0'>
           <Contact />
-          <StarsCanvas />
+          {/* <StarsCanvas /> */}
         </div>
       </div>
     </BrowserRouter>
