71af1da7a3a68b484a517b7b270fe9761bce837c
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2025-09-01 13:51:23 +0300

    updated

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,7 +6,7 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <div className='hidden md:flex md:flex-row md:flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
